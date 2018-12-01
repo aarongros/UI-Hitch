692a882df7a4add311ea27746554219b8517a4b2
aarongros <aaron.gros@hotmail.com>
2018-11-30 19:05:37 -0600

    added a list to shedule page

--- flask-test/app.py
+++ flask-test/app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+stations = [
+	{
+		'name': 'Ike',
+		'time': '2'
+	},
+	{
+		'name': 'Transit',
+		'time': '5'
+	},
+	{
+		'name': 'Union',
+		'time': '1'
+	}
+]
+
 @app.route('/')
 def home():
 	return render_template('home.html')
@@ -11,7 +26,7 @@ def about():
 
 @app.route('/schedule')
 def schedule():
-	return render_template('schedule.html')
+	return render_template('schedule.html', stations=stations)
 
 @app.route('/signup')
 def signup():
