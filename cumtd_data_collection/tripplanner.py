58cb1cf7f0581c4bf8c21a4ac871d45eef196528
Shaw Kagawa <skagawa2@illinois.edu>
2018-12-10 17:31:59 -0600

    tripplanner starter code

--- cumtd_data_collection/tripplanner.py
+++ cumtd_data_collection/tripplanner.py
@@ -1,1 +1,9 @@
 # use gettripsbylatlon to find trips
+
+from cumtd_data_collection import cumtd_request_url
+import requests
+
+class TripPlanner:
+    def __init__(self):
+        # to make call:
+        # requests.get(cumtd_request_url(methodname, {'extra_params': param}))
