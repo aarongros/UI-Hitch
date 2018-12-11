f31d456ee863fba5f6dadbee7f1f4ed5edd5229c
Shaw Kagawa <skagawa2@illinois.edu>
2018-12-11 13:41:09 -0600

    turns out i was getting the wrong information before, this one should be correct. will crash if no more trips left after filter

--- cumtd_data_collection/tripplanner.py
+++ cumtd_data_collection/tripplanner.py
@@ -3,8 +3,7 @@
 import googlemaps
 from apikey import keys
 from cumtd_data_collection import cumtd_request_url
-import requests, sys, copy, json, csv
-from pprint import pprint
+import requests, sys, copy, json, csv, pprint
 from datetime import datetime, timedelta
 
 
@@ -12,6 +11,7 @@ class TripPlanner:
     def __init__(self, start, end):
         self._orig = start
         self._dest = end
+        
         
     def get_directions(self):
         return self._sorted_trips
@@ -27,12 +27,17 @@ class TripPlanner:
         # 4) sort by total travel time
         
         trips = self._get_trip()
+        if len(trips) == 0:
+            raise Exception("no trips available")
         self._old_trips = copy.deepcopy(trips)
         
         for trip in trips:
             self._edit_trip(trip)
 
-        self._trips = filter(self._find_inconsistencies, trips)
+        self._trips = list(filter(self._find_inconsistencies, trips))
+        if len(self._trips) == 0 and len(self._old_trips) != 0:
+            pprint.pprint(self._old_trips)
+            raise Exception("no filtered trips available, returned old")
 
         for trip in trips:
             self._find_travel_time(trip)
@@ -48,13 +53,14 @@ class TripPlanner:
                 
                 # begin transit leg
                 stop_name = transit['departure_stop']['name']
-                begin_stop_id = self._to_stop_id(stop_name)
+                stop_loc = transit['departure_stop']['location']
+                begin_stop_id = self._to_stop_id(stop_name, stop_loc['lat'], stop_loc['lng'])
                 departures = self._get_departures_by_stop(begin_stop_id)
                 for departure in departures['departures']:
-                    if departure['route']['route_id'] == transit['line']['name']:
-                        real_time = departure['scheduled']
+                    if departure['route']['route_id'].lower() == transit['line']['name'].lower():
+                        real_time = departure['expected']
                         real_time_full = self._strptime(real_time)
-                        real_time_24hr = datetime.strftime(real_time_full, "%I:%M%p")
+                        real_time_12hr = datetime.strftime(real_time_full, "%I:%M:%S%p")
                         real_time_int = int(real_time_full.timestamp())
                         # put them back into the schedule
                         transit['departure_time'] = {
@@ -66,13 +72,14 @@ class TripPlanner:
 
                 # end transit leg
                 stop_name = transit['arrival_stop']['name']
-                end_stop_id = self._to_stop_id(stop_name)
+                stop_loc = transit['arrival_stop']['location']
+                end_stop_id = self._to_stop_id(stop_name, stop_loc['lat'], stop_loc['lng'])
                 departures = self._get_departures_by_stop(end_stop_id)['departures']
                 for departure in departures:
-                    if departure['route']['route_id'] == transit['line']['name']:
-                        real_time = departure['scheduled']
+                    if departure['route']['route_id'].lower() == transit['line']['name'].lower():
+                        real_time = departure['expected']
                         real_time_full = self._strptime(real_time)
-                        real_time_24hr = datetime.strftime(real_time_full, "%I:%M%p")
+                        real_time_12hr = datetime.strftime(real_time_full, "%I:%M%p")
                         real_time_int = int(real_time_full.timestamp())
                         # put them back into the schedule
                         transit['arrival_time'] = {
@@ -81,22 +88,32 @@ class TripPlanner:
                             'value': real_time_int
                         }
                         break
+                duration = transit['arrival_time']['value'] - transit['departure_time']['value']
+                leg['duration']['value'] = duration
+                leg['duration']['text'] = "{0:.2f} mins".format(duration / 60)
         # end time doesn't change UNLESS last leg is bus
         # maybe do another call to API to figure out whether person can walk from bus endpoint
         # to destination?
 
 
-    def _to_stop_id(self, stop_name):
+    def _to_stop_id(self, stop_name, lat, lon):
         stop_name = stop_name.replace(" and ", " & ")
         with open('google_transit/stops.txt', 'rt') as f:
             csv_reader = csv.reader(f)
+            next(csv_reader, None) # skip header line
             for row in csv_reader:
+                row_lat = float(row[4])
+                row_lon = float(row[5])
                 if row[2] == stop_name:
+                    # column stop_name
+                    return row[0]
+                elif abs(row_lat - lat) < 1e-4 and abs(row_lon - lon) < 1e-4:
+                    # colum stop_lon
                     return row[0]
         raise Exception("stop {} not found!".format(stop_name))
 
     def _text_to_seconds(self, text):
-        return int(text.split(" min")[0])
+        return float(text.split(" min")[0])
 
 
     def _find_inconsistencies(self, trip):
@@ -107,6 +124,8 @@ class TripPlanner:
             elif leg['travel_mode'] == 'TRANSIT':
                 transit = leg['transit_details']
                 if transit['departure_time']['value'] < times[-1]:
+                    print("filtered out this trip, bus came early?")
+                    pprint.pprint(trip)
                     return False
                 else:
                     times.append(transit['arrival_time']['value'])
@@ -118,14 +137,17 @@ class TripPlanner:
         for leg in trip['legs'][0]['steps']:
             # both TRANSIT and WALKING have same 'duration' object
             # leg['duration']['text'] is in format: "12 mins" --> convert to seconds
-            total_travel_time += self._text_to_seconds(leg['duration']['text'])
+            total_travel_time += leg['duration']['value']
         trip['travel_time'] = {'value': total_travel_time,
-                               'text': "{} mins".format(total_travel_time / 60)}
-        # trip['legs'][0]['duration'] = trip['legs'][0]['arrival_time']['value'] - trip['legs'][0]['departure_time']['value']
+                               'text': "{0:.2f} mins".format(total_travel_time / 60)}
+        total_time = trip['legs'][0]['arrival_time']['value'] - trip['legs'][0]['departure_time']['value']
+        wait_time = total_time - total_travel_time
+        trip['wait_time'] = {'value': wait_time,
+                               'text': "{0:.2f} mins".format(wait_time / 60)}
 
     def _sort_trips_by_time(self):
-        sorted_by_wait = sorted(self._trips, key=lambda x: x['travel_time'])
-        self._sorted_trips = sorted(sorted_by_wait, key=lambda x: x['legs'][0]['duration']['value'])
+        sorted_by_wait = sorted(self._trips, key=lambda x: x['wait_time'])
+        self._sorted_trips = sorted(sorted_by_wait, key=lambda x: x['travel_time'])
 
     def _strptime(self, time):
         # parse times, should all be in same format
@@ -147,13 +169,17 @@ class TripPlanner:
             raise Exception("error calling CUMTD API: {}".format(result_json['status']['msg']))
         return r.json()
 
+    def pprint_to_file(self, obj, fout):
+        pp = pprint.PrettyPrinter(stream=open(fout,'w'))
+        pp.pprint(obj)
+
 def main():
     start = "Siebel Center for Computer Science"
     end = "The University of Illinois - Memorial Stadium"
     trip_planner = TripPlanner(start, end)
     trip_planner.search()
-    pprint(trip_planner.get_old_directions())
-    pprint(trip_planner.get_directions())
+    trip_planner.pprint_to_file(trip_planner.get_old_directions(), "old_directions.txt")
+    pprint.pprint(trip_planner.get_directions())
 
 if __name__ == "__main__":
     sys.exit(main())
